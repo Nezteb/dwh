f26edb5ef6548e6639f64aad0fbe00c49d7cefbc
Noah Betzen <noah@nezteb.net>
2021-05-01 14:14:19 -0500

    Gigalixir config changes

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :homework, HomeworkWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOST_INDEX") || "localhost", port: System.get_env("PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :homework, Homework.Repo,
